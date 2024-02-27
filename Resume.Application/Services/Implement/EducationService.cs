e4216b8aca4f363e3363b5421e07c008f2dd6350
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-02-27 15:20:37 -0800

    Initial IEducationServicse&IEducationServicse

---
+++ Resume.Application/Services/Implement/EducationService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resume.Application.Services.Interface;
+using Resume.Presentation.Models.Entities.Educaties;
+using Resume.Presentation.Models.ResumeDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Services.Implement
+{
+    public class EducationService:IEducationService
+    {
+        #region ctor
+        private RsumeDbContext _context;
+        public EducationService(RsumeDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+        public async Task CreatAnEducaties(Educaties educaties)
+        {
+          await  _context.AddAsync(educaties);
+          await _context.SaveChangesAsync();
+           
+        }
+
+        public async Task DeleteAnEducaties(int id)
+        {
+            var ed =await GetbyIDAnEducaties(id);
+             _context.Remove(ed);
+           await _context.SaveChangesAsync();
+
+        }
+
+        public async Task EditeAnEducaties(int id)
+        {
+            var ed = await GetbyIDAnEducaties(id);
+            _context.Update(ed);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Educaties>> GetAllAnEducaties()
+        {
+          List<Educaties> educaties=await _context.Educaties.ToListAsync();
+            return educaties;
+        }
+
+        public async Task<Educaties> GetbyIDAnEducaties(int id)
+        {
+           var educaties= await _context.Educaties.Where(e=>e.id==id).FirstAsync();
+            return educaties;
+        }
+    }
+}
---
+++ Resume.Application/Services/Interface/IEducationService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Presentation.Models.Entities.Educaties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Services.Interface
+{
+    public interface IEducationService
+    {
+        Task CreatAnEducaties(Educaties educaties);
+        Task DeleteAnEducaties(int id);
+        Task EditeAnEducaties(int id);
+        Task<Educaties> GetbyIDAnEducaties(int id);
+        Task<List<Educaties>> GetAllAnEducaties();
+    }
+}
