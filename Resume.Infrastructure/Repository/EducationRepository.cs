00ee321a589fa0b3b5baf9f42e707e202829718e
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-03-14 17:49:37 -0700

    add Reposetori

---
+++ Resume.Infrastructure/Repository/EducationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Resume.Domain.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.Infrastructure.Repository
+{
+    public class EducationRepository:IEducationRepository
+    {
+    }
+}
