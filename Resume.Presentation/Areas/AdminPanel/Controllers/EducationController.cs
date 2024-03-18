c8346cf35a0b0e02c6ba3e6adf97c0babbfa9a51
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-03-17 18:06:53 -0700

    add-Areas

---
+++ Resume.Presentation/Areas/AdminPanel/Controllers/EducationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Application.Services.Interface;
+
+namespace Resume.Presentation.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class EducationController : Controller
+    {
+        #region Ctor
+        private readonly IEducationService _educationService;
+        public EducationController (IEducationService educationService)
+        {
+            _educationService = educationService;
+        }
+        #endregion
+        #region List Of Education
+        [HttpGet]
+        public async Task<IActionResult> ListOfEducations()
+        {
+            var model = _educationService.GetAllAnEducaties;
+            return View(model);
+        }
+        #endregion
+        #region Create An Education
+
+        #endregion
+        #region Edite an Education
+
+        #endregion
+        #region Delete an Education
+
+        #endregion
+    }
+}
---
+++ Resume.Presentation/Areas/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class HomeController : Controller
+    {
+ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
